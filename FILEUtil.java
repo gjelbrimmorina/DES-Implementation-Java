dffc58317bd4fab21caec7574c3f1095671791fa
gjelbrimmorina <gjelbrim.morina@student.uni-pr.edu>
2026-04-19 12:00:55 +0200

    Update FILEUtil.java
    File encryption/decryption using DES with improved handling to avoid corruption.

--- FILEUtil.java
+++ FILEUtil.java
@@ -1,8 +1,13 @@
 import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
 import javax.crypto.SecretKey;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
+/**
+ * Handles DES encryption and decryption for files.
+ */
 public class FileUtil {
 
     public static void encryptFile(String inputFile, String outputFile, SecretKey key) throws Exception {
@@ -11,40 +16,35 @@ public class FileUtil {
 
         FileInputStream fis = new FileInputStream(inputFile);
         FileOutputStream fos = new FileOutputStream(outputFile);
-
         CipherOutputStream cos = new CipherOutputStream(fos, cipher);
 
-byte[] buffer = new byte[4096];
-int bytesRead;
+        byte[] buffer = new byte[4096];
+        int bytesRead;
 
-while ((bytesRead = fis.read(buffer)) != -1) {
-    cos.write(buffer, 0, bytesRead);
-}
+        while ((bytesRead = fis.read(buffer)) != -1) {
+            cos.write(buffer, 0, bytesRead);
+        }
 
-cos.close();
-
+        cos.close();
         fis.close();
-        fos.close();
     }
 
     public static void decryptFile(String inputFile, String outputFile, SecretKey key) throws Exception {
-Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+        Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE, key);
 
         FileInputStream fis = new FileInputStream(inputFile);
+        CipherInputStream cis = new CipherInputStream(fis, cipher);
         FileOutputStream fos = new FileOutputStream(outputFile);
 
-        CipherOutputStream cos = new CipherOutputStream(fos, cipher);
+        byte[] buffer = new byte[4096];
+        int bytesRead;
 
-byte[] buffer = new byte[4096];
-int bytesRead;
+        while ((bytesRead = cis.read(buffer)) != -1) {
+            fos.write(buffer, 0, bytesRead);
+        }
 
-while ((bytesRead = fis.read(buffer)) != -1) {
-    cos.write(buffer, 0, bytesRead);
-}
-
-cos.close();
-        fis.close();
+        cis.close();
         fos.close();
     }
 }
